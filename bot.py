d1789e37283ee55e4051f54db8c994f9e4564edc
D0N-B0T <salinasjoost@gmail.com>
2022-06-28 14:34:47 -0400

    added more functions and tba command

--- bot.py
+++ bot.py
@@ -14,6 +14,25 @@ def send_welcome(message):
     bot.send_message(message.chat.id, "To start, send me a domain with /domain <url>.\n\n")
 
 
+@bot.message_handler(commands=['tba'])
+def tba_command(message):
+    bot.send_message(message.chat.id, """
+    →Seclists\n
+    →Hash-identifier\n
+    →XSSMAP\n
+    →Smuggler\n
+    →SSRFmap\n
+    →Gmapsapiscanner\n
+    →Qsreplace\n
+    →exiftool\n
+    →XSRFProbe\n
+    →XXE Exploiter\n
+    →Rush\n
+    →Rustscan\n
+    →LFISuite\n
+    →Wapiti\n
+    →Nuclei\n
+    """)
 
 #def domain command
 @bot.message_handler(commands=['domain'])
@@ -22,23 +41,27 @@ def send_welcome(message):
     send_welcome.args = args.split(' ')[1]
     bot.send_message(message.chat.id, "Running... Please wait.\n\n")
     bot.send_message(message.chat.id, "The whole process can take a while.\n\n")
+    #process
     bot.send_message(message.chat.id, "[1] + Starting Sonar search with Rapid7.\n\n")
     send_welcome.process = sonar_rapid7()
     bot.send_message(message.chat.id, "[2] + Starting DNSX subdomain brute force.\n\n")
+    send_welcome.process = dnsx()
     bot.send_message(message.chat.id, "[3] + Starting subdomain enumeration...")  
     send_welcome.process = subfinder()
-    
-    
-    bot.send_message(message.chat.id, "Done.\n\n")
-    bot.send_document(message.chat.id, open(send_welcome.args + '.subfinder.txt', 'rb'))
-
     bot.send_message(message.chat.id, "[4] + Starting gau + unfurl.\n\n")
+    send_welcome.process = gau()
     bot.send_message(message.chat.id, "[5] + Starting get ip address.\n\n")
+    send_welcome.process = dnsx2()
     bot.send_message(message.chat.id, "[6] + Starting reverse dns.\n\n")
+    send_welcome.process = rapid72()
     bot.send_message(message.chat.id, "[7] + Starting nrich portscan common cves.\n\n")
+    send_welcome.process = shodan()
     bot.send_message(message.chat.id, "[8] + Starting portscan.\n\n")
+    send_welcome.process = naabu()
     bot.send_message(message.chat.id, "[9] + Starting check active domains.\n\n")
+    send_welcome.process = httpx_check()
     bot.send_message(message.chat.id, "[10] + Starting nuclei attack.\n\n")
+    send_welcome.process = nuclei()
     bot.send_message(message.chat.id, "BotBounty work is done. Sending you the results.\n\n")
     #bot.send_document(message.chat.id, open(send_welcome.args + '.subfinder.txt', 'rb'))
     bot.send_message(message.chat.id, "Done. Now you can send me a new domain with /domain <url>.")
@@ -48,11 +71,11 @@ def send_welcome(message):
 
 #0 sonar search tld
 def sonar_rapid7():
-    return
-
+    os.system('curl -k -s https://sonar.omnisint.io/subdomains/{args} | jq -r ‘.[]’ | sort -u > {args}.sonar.txt'.format(send_welcome.args, args=send_welcome.args))
+    
 #1 dnsx sub brute
 def dnsx():
-    return
+    os.system('dnsx -silent -d {args} -w -o {args}.dnsx.txt'.format(send_welcome.args, args=send_welcome.args))
 
 #2 subfinder subdomain enumeration
 def subfinder():
@@ -64,11 +87,12 @@ def subfinder():
 
 #3 gau + unfurl
 def gau():
-    return
+    os.systen('gau {args} --blacklist png,jpg,gif -o {args}.gau.txt'.format(send_welcome.args, args=send_welcome.args))
 
 #4 get ip address
 def dnsx2():
-    return
+    os.system('dnsx -silent -a -resp-only -l {args}.subfinder.txt'.format(send_welcome.args, args=send_welcome.args))
+
 
 #5 reverse dns 
 def rapid72():
@@ -88,7 +112,7 @@ def httpx_check():
 
 #9 nuclei attack
 def nuclei():
-    return
+    os.system('nuclei ')
 
 
 
@@ -117,8 +141,9 @@ def getastra(message):
 @bot.message_handler(commands=['arjun'])
 def getarjun(message):
     bot.send_message(message.chat.id, "Running... Please wait.\n\n")
-    os.system('arjun -u {} > arjun.txt'.format(message.text[7:]))
+    os.system('arjun -u {} -oT arjun.txt'.format(message.text[7:]))
     bot.send_document(message.chat.id, open('arjun.txt', 'rb'))
+    os.system('rm arjun.txt')   
     bot.send_message(message.chat.id, "Done!")
 
 
